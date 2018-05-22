3024a132788c6e345a4e8f73d8bce1923c94af8c
Jesse Ma <j.masonoma17@gmail.com>
2018-05-21 23:41:53 -0700

    Relocate multer, multer-gridfs-storage configurations to routes.js from server.js

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,5 +1,32 @@
 const mongoose = require('mongoose');
+const Image = mongoose.model('Image');
+const Images = require('../controllers/images.js');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
 
 module.exports = function(app) {
-    
-}
+    // Set up multer-gridfs-storage
+    const storage = new GridFsStorage({
+        url: 'mongodb://localhost:27017/gridFS',
+        file: function(request, file) {
+            return 'file_' + Date.now();
+        }
+    })
+
+    // Configure multer for single upload
+    const upload = multer({
+        storage: storage
+    }).single('file');
+
+    // Upload image
+    app.post('/upload', function(request, response) {
+        upload(request, response, function(error) {
+            if (error) {
+                console.log(error);
+                response.json({status: 400, error: error});
+            }
+            console.log(request.file);
+            response.json({status: 200});
+        })
+    });
+};
