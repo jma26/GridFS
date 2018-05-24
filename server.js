959f2f5a4dd3265caac10b0e2a829d06c3075538
Jesse Ma <j.masonoma17@gmail.com>
2018-05-23 20:07:31 -0700

    Remove angular project, Add views folder container ejs files

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express = require('express');
 const bodyParser = require('body-parser');
+const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 
 // Initialize express
 const app = express();
+
+// Set View engine
+app.set('view engine', 'ejs');
 
 // Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,16 +16,8 @@ app.use(bodyParser.json());
 // Require mongoose.js to connect to mongoDB
 require('./server/config/mongoose.js');
 
-// Angular app
-app.use(express.static(path.join(__dirname, '/gridfsAng/dist')));
-
 // Require routes.js for server routing
 const route = require('./server/config/routes.js')(app);
-
-// Catch all route to Angular
-app.all("*", function(request, response, next) {
-    response.send(path.resolve("./gridfsAng/dist/index.html"))
-});
 
 // Port Number
 const port = 8000;
